bf8ca6e904b68568de75d5b4a2a43f0bb918001b
Noah Phillips <noahphillips@Noahs-MacBook-Pro.local>
2018-03-19 11:28:27 -0400

    updated the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueSocketio from 'vue-socket.io';
+import io from 'socket.io-client';
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld,
+            beforeEnter (to, from, next) {
+                if (!Vue.prototype.$socket) {
+                    Vue.use(VueSocketio, io("http://localhost:8000/", {}));
+                }
+                next()
+            }
+        }
+    ]
 })
