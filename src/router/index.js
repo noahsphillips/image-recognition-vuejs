d2b4ac528e498233c43a3b72702d5b98b3839d62
Noah Phillips <noahphillips@Noahs-MacBook-Pro.local>
2018-04-09 12:51:22 -0400

    created form component

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import Router from 'vue-router'
 import VueSocketio from 'vue-socket.io';
 import io from 'socket.io-client';
 import ImageFrame from '@/components/ImageFrame';
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
 Vue.use(Router)
 
 export default new Router({
